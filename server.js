9d6b112f5f1903c10e748c2084dda502b10119c9
spacemen0 <frakctures@outlook.com>
2024-09-11 16:25:11 +0200

    add cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import messages from "./routes/messages.js"
 import notFound from './middlewares/notFound.js';
 import logger from './middlewares/logger.js';
+import cors from 'cors'
 const app = express();
 app.use(logger);
 app.use(express.json());
+app.use(cors())
 app.use('/messages', messages);
 app.use(notFound)
 
